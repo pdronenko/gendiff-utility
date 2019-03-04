04c9b0c939bbffe3f4c81d981d01513462efaae5
pdronenko <pdronenko@ya.ru>
2019-03-04 13:39:09 +0300

    test

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,10 +4,6 @@ const program = require('commander');
 program
   .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1')
-  .option('-h, --help', 'output usage information')
-  .option('-V, --version', 'output the version number')
   .option('-f, --format [type]', 'Output format');
 
 program.parse(process.argv);
-program.help();
