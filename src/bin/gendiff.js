2c292aa8c770c8f4e3661c066391a96e4c4e4b45
pdronenko <pdronenko@ya.ru>
2019-03-04 12:44:46 +0300

    init gendiff bin

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,1 +1,12 @@
 #!/usr/bin/env node
+
+const program = require('commander');
+
+program
+  .option('-h, --help', 'output usage information')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'Output format');
+
+
+
+program.parse(process.argv);
