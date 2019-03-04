3cc4fc8790353330997249902ee6a3b7e33b34f9
pdronenko <pdronenko@ya.ru>
2019-03-04 20:51:31 +0300

    refactor code

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -11,12 +11,12 @@ program
   .version(version)
   .option('-f, --format [type]', 'Output format')
   .action((firstConfig, secondConfig) => {
-    const fileCheck = (err, data) => err ? console.error(err) : data;
+    const fileCheck = (err, data) => (err ? console.error(err) : data);
     const parse = path => JSON.parse(fs.readFileSync(path, 'UTF-8', fileCheck));
     const firstConfigData = parse(firstConfig);
     const secondConfigData = parse(secondConfig);
 
-    return gendiff(parse(firstConfigData, secondConfigData);
+    return gendiff(parse(firstConfigData, secondConfigData));
   });
 
 program.parse(process.argv);
