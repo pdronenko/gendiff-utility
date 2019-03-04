eadb5bb14c622fbb891fb2e5a951352407fef49a
pdronenko <pdronenko@ya.ru>
2019-03-04 14:09:32 +0300

    add version

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
+import { version } from '../../package.json';
+
 const program = require('commander');
 
 program
   .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.2.0')
+  .version(version)
   .option('-f, --format [type]', 'Output format');
 
 program.parse(process.argv);
