2ae0bbc8641621661a6c9285aa2a86db5074f492
pdronenko <pdronenko@ya.ru>
2019-03-04 13:28:07 +0300

    test

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,16 +1,12 @@
 #!/usr/bin/env node
-import * as library from '../index';
-
 const program = require('commander');
 
 program
-  .command('<firstConfig> <secondConfig>')
+  .arguments('<firstConfig> <secondConfig>')
+  .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .option('-h, --help', 'output usage information')
-  .description(library.description())
   .option('-V, --version', 'output the version number')
   .option('-f, --format [type]', 'Output format');
 
-
-program.outputHelp();
 program.parse(process.argv);
--- src/index.js
+++
@@ -1,1 +0,0 @@
-export const description = () => 'Compares two configuration files and shows a difference.';
