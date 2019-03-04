4b8e5a062f16dc24b7effbdd845faa43f5b1eb6a
pdronenko <pdronenko@ya.ru>
2019-03-04 13:22:35 +0300

    test

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,11 +4,13 @@ import * as library from '../index';
 const program = require('commander');
 
 program
+  .command('<firstConfig> <secondConfig>')
+  .version('0.0.1');
   .option('-h, --help', 'output usage information')
   .description(library.description())
   .option('-V, --version', 'output the version number')
-  .option('-f, --format [type]', 'Output format')
-  .command('<firstConfig> <secondConfig>');
+  .option('-f, --format [type]', 'Output format');
 
-program.version('0.0.1');
+
+program.outputHelp();
 program.parse(process.argv);
