f23a9e96345cbc16886e09bde74ecc02574276aa
pdronenko <pdronenko@ya.ru>
2019-03-04 13:06:35 +0300

    test

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,12 +6,8 @@ const program = require('commander');
 program
   .option('-h, --help', 'output usage information')
   .option('-V, --version', 'output the version number')
-  .option('-f, --format [type]', 'Output format');
-
-  program
-    .command('')
-    .option("<firstConfig> <secondConfig>")
-
+  .option('-f, --format [type]', 'Output format')
+  .command('<firstConfig> <secondConfig>');
 
 program.parse(process.argv);
 
