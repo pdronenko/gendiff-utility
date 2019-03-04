3e61aa52974da6fb306af92087406d2f259da69e
pdronenko <pdronenko@ya.ru>
2019-03-04 13:13:05 +0300

    test

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,8 +5,10 @@ const program = require('commander');
 
 program
   .option('-h, --help', 'output usage information')
+  .description(library.description())
   .option('-V, --version', 'output the version number')
   .option('-f, --format [type]', 'Output format')
   .command('<firstConfig> <secondConfig>');
 
+program.version(pckg.version);
 program.parse(process.argv);
