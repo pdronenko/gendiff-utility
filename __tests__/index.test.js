8e8c89463a4611a14faebe7b08cc680a199c1f19
pdronenko <pdronenko@ya.ru>
2019-03-06 15:10:43 +0300

    refactor tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,16 +1,17 @@
 import fs from 'fs';
 import gendiff from '../src';
 
-const expectedDiff = () => fs.readFileSync('__tests__/__fixtures__/expectedResult', 'UTF-8');
 const pathToInitialBefore = '__tests__/__fixtures__/initial/before';
 const pathToInitialAfter = '__tests__/__fixtures__/initial/after';
 const extnameTable = [['.json'], ['.yml'], ['.ini']];
 
 describe('gendiff', () => {
+  const expectedDiff = fs.readFileSync('__tests__/__fixtures__/expectedResult', 'UTF-8');
+
   test.each(extnameTable)('test %s', (extname) => {
     const pathToBeforeFile = `${pathToInitialBefore}${extname}`;
     const pathToAfterFile = `${pathToInitialAfter}${extname}`;
     const receivedDiff = gendiff(pathToBeforeFile, pathToAfterFile);
-    expect(receivedDiff).toBe(expectedDiff());
+    expect(receivedDiff).toBe(expectedDiff);
   });
 });
