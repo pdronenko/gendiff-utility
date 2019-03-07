618ddc6c5e1d52e633a7a1e6135597ac4f4bf949
pdronenko <pdronenko@ya.ru>
2019-03-07 14:31:28 +0300

    git add render logic

--- src/index.js
+++ src/index.js
@@ -43,15 +43,38 @@ const buildAST = (beforeData, afterData) => {
   const keys = union(Object.keys(beforeData), Object.keys(afterData));
   return keys.map((key) => {
     const { status, value, children } = astTree.find(({ check }) => check(key));
-    return { name: key, status, value: value(key), children: children(key) };
+    return { key, status, value: value(key), children: children(key) };
   });
 };
 
 export default (pathToFile1, pathToFile2) => {
-  const signList = { noChange: ' ', deleted: '-', added: '+' };
-  const buildLine = (sign, key, value) => `  ${signList[sign]} ${key}: ${value}`;
   const beforeData = parse(readDataFromFile(pathToFile1), path.extname(pathToFile1));
   const afterData = parse(readDataFromFile(pathToFile2), path.extname(pathToFile2));
 
-  return JSON.stringify(buildAST(beforeData, afterData));
+  const stringify = (value, tab) => {
+    if (typeof value === 'string') return value;
+    const addTab = tab.repeat(2);
+    const entriesString = Object.entries(value).map(([key, value]) => `${key}: ${value}`).join(`\n${addTab}`);
+    return `{\n${addTab}${entriesString}\n${tab}}`;
+  };
+
+  const renderList = { 
+    noChange: (key, value) => `    ${key}: ${JSON.stringify(value)}`, 
+    changed: (key, value) => {
+      const deletedLine = `  - ${key}: ${JSON.stringify(value[0])}`;
+      const addedLine = `  + ${key}: ${JSON.stringify(value[1])}`;
+      return [deletedLine, addedLine];
+    }, 
+    deleted: (key, value, tab) => `  - ${key}: ${stringify(value, '  '.repeat(2))}`,
+    added: (key, value) => `  + ${key}: ${stringify(value, '  '.repeat(2))}`,
+    children: () => 'children',
+  };
+  const render = (ast, tab = '') => {
+    const mappedAst = flatten(ast.map(({ key, value, status, children }) => {
+      return renderList[status](key, value, tab);
+    }));
+    return `{\n${mappedAst.join('\n')}\n}`;
+  };
+//  return buildAST(beforeData, afterData);
+  return render(buildAST(beforeData, afterData));
 };
