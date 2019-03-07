4e6a97d69aad34756049e49e93f4caffd36380dc
pdronenko <pdronenko@ya.ru>
2019-03-07 20:56:43 +0300

    add ast render code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-/* eslint-disable */
 import { has, union, flatten } from 'lodash';
-import parse from './parsers';
-import path from 'path';
 import fs from 'fs';
+import path from 'path';
+import parse from './parsers';
 
 const readDataFromFile = pathToFile => fs.readFileSync(pathToFile, 'UTF-8');
 
@@ -10,33 +9,33 @@ const buildAST = (beforeData, afterData) => {
   const astTree = [
     {
       status: 'noChange',
-      value: (key) => beforeData[key],
+      value: key => beforeData[key],
       children: () => [],
-      check: (key) => has(beforeData, key) && beforeData[key] === afterData[key],
+      check: key => has(beforeData, key) && beforeData[key] === afterData[key],
     },
     {
-      status: 'children',
+      status: 'hasChildren',
       value: () => '',
-      children: (key) => buildAST(beforeData[key], afterData[key]),
-      check: (key) => beforeData[key] instanceof Object && afterData[key] instanceof Object,
+      children: key => buildAST(beforeData[key], afterData[key]),
+      check: key => beforeData[key] instanceof Object && afterData[key] instanceof Object,
     },
     {
       status: 'deleted',
-      value: (key) => beforeData[key],
+      value: key => beforeData[key],
       children: () => [],
-      check: (key) => has(beforeData, key) && !has(afterData, key),
+      check: key => has(beforeData, key) && !has(afterData, key),
     },
     {
       status: 'changed',
-      value: (key) => [beforeData[key], afterData[key]],
+      value: key => [beforeData[key], afterData[key]],
       children: () => [],
-      check: (key) => has(beforeData, key) && beforeData[key] !== afterData[key],
+      check: key => has(beforeData, key) && beforeData[key] !== afterData[key],
     },
     {
       status: 'added',
-      value: (key) => afterData[key],
+      value: key => afterData[key],
       children: () => [],
-      check: (key) => !has(beforeData, key) && has(afterData, key),
+      check: key => !has(beforeData, key) && has(afterData, key),
     },
   ];
 
@@ -51,30 +50,32 @@ export default (pathToFile1, pathToFile2) => {
   const beforeData = parse(readDataFromFile(pathToFile1), path.extname(pathToFile1));
   const afterData = parse(readDataFromFile(pathToFile2), path.extname(pathToFile2));
 
-  const stringify = (value, tab) => {
-    if (typeof value === 'string') return value;
-    const addTab = tab === '' ? '  ' : tab.repeat(2);
-    const entriesString = Object.entries(value).map(([key, value]) => `${key}: ${value}`).join(`\n${addTab}`);
-    return `{\n${addTab}${entriesString}\n${tab}}`;
+  const signList = { noChange: ' ', deleted: '-', added: '+', hasChildren: ' ' };
+  const buildLine = (status, key, value, spaceCount) => {
+    const stringify = (data) => {
+      if (typeof data !== 'object') return data;
+      const newSpaceCount = spaceCount + 4;
+      const entriesString = Object.entries(data)
+        .map(([k, v]) => buildLine('noChange', k, v, newSpaceCount)).join('\n');
+      return `{\n${entriesString}\n${' '.repeat(newSpaceCount)}}`;
+    };
+
+    if (status === 'changed') {
+      const deletedLine = buildLine('deleted', key, value[0], spaceCount);
+      const addedLine = buildLine('added', key, value[1], spaceCount);
+      return [deletedLine, addedLine];
+    }
+    return `${' '.repeat(spaceCount)}  ${signList[status]} ${key}: ${stringify(value)}`;
   };
 
-  const renderList = { 
-    noChange: (key, value, tab) => `    ${key}: ${stringify(value, tab.repeat(2))}`, 
-    changed: (key, value, tab) => {
-      const deletedLine = `  - ${key}: ${stringify(value[0], tab.repeat(2))}`;
-      const addedLine = `  + ${key}: ${stringify(value[1], tab.repeat(2))}`;
-      return [deletedLine, addedLine];
-    }, 
-    deleted: (key, value, tab) => `  - ${key}: ${stringify(value, tab.repeat(2))}`,
-    added: (key, value, tab) => `  + ${key}: ${stringify(value, tab.repeat(2))}`,
+  const render = (astTree, spaceCount = 0) => {
+    const resultString = flatten(astTree
+      .map(({ key, value, status, children }) => {
+        const newValue = status === 'hasChildren' ? render(children, spaceCount + 4) : value;
+        return buildLine(status, key, newValue, spaceCount);
+      }));
+    return `{\n${resultString.join('\n')}\n${' '.repeat(spaceCount)}}`;
   };
-  const render = (ast, tab = '') => {
-    const mappedAst = flatten(ast.map(({ key, value, status, children }) => {
-      if (status === 'children') return `    ${key}: ${render(children, '  ')}`;
-      return renderList[status](key, value, tab === '' ? '  ' : tab.repeat(2));
-    }));
-    return `{\n${mappedAst.join('\n')}\n}`;
-  };
-//  return buildAST(beforeData, afterData);
+
   return render(buildAST(beforeData, afterData));
 };
