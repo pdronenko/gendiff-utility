8cb0ae952f606c6ee62607001714d2945cc7c6c3
pdronenko <pdronenko@ya.ru>
2019-03-08 22:12:35 +0300

    refactor code

--- src/renderers/plainRender.js
+++ src/renderers/plainRender.js
@@ -14,11 +14,12 @@ const plainRender = (ast, path = '') => ast
     const {
       key, value, type, children, delValue, addValue,
     } = node;
+    const dot = path === '' ? '' : '.';
     if (type === 'unchanged') return null;
     if (type === 'nested') {
-      return plainRender(children, `${path}${path === '' ? '' : '.'}${key}`);
+      return plainRender(children, `${path}${dot}${key}`);
     }
-    return buildLine(type, `${path}.${key}`, value, delValue, addValue);
+    return buildLine(type, `${path}${dot}${key}`, value, delValue, addValue);
   }).filter(n => n !== null).join('\n');
 
 export default plainRender;
