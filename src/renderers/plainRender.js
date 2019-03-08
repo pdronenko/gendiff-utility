d2adebd40be3dcca56b4bee01edb63c0c16941a1
pdronenko <pdronenko@ya.ru>
2019-03-08 21:52:56 +0300

    add plain render

--- src/renderers/plainRender.js
+++ src/renderers/plainRender.js
@@ -1,1 +1,30 @@
-export default () => 'test';
+import { flatten } from 'lodash';
+
+const stringify = data => typeof data !== 'object' ? data : '[complex value]';
+
+const signList = {
+  changed: (v, delValue, addValue) => `was updated. From ${delValue} to ${addValue}`,
+  added: (value) => `was added with value: ${value}`,
+  deleted: () => 'was removed',
+  nested: () => 'was nested',
+};
+
+const buildLine = (type, key, value, delValue, addValue) => {
+    return `Property '${key}' ${signList[type](stringify(value), stringify(delValue), stringify(addValue))}`;
+};
+
+
+const plainRender = (ast, path = '') => {
+  return ast.map((node) => {
+    const {
+      key, value, type, children, delValue, addValue,
+    } = node;
+    if (type === 'unchanged') return null;
+    if (type === 'nested') {
+      return plainRender(children, `${path}${path === '' ? '' : '.'}${key}`);
+    }
+    return buildLine(type, `${path}.${key}`, value, delValue, addValue);
+  }).filter(n => n !== null).join('\n');
+};
+
+export default plainRender;
