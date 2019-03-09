dd5efd2dff5d2a7b34d4248f03783502d45e239b
pdronenko <pdronenko@ya.ru>
2019-03-09 20:21:38 +0300

    refactor code

--- src/renderers/visualRender.js
+++ src/renderers/visualRender.js
@@ -1,43 +1,66 @@
 import { flatten } from 'lodash';
 
-const signList = {
-  unchanged: ' ', deleted: '-', added: '+', nested: ' ',
-};
+const genSpaces = depth => ' '.repeat(depth * 4);
 
-const stringify = (data, spaceCount) => {
+const stringify = (data, depth) => {
   if (typeof data !== 'object') return data;
 
-  const newSpaceCount = spaceCount + 4;
   const entriesString = Object.entries(data)
-    .map(([key, value]) => {
-      const newValue = typeof value === 'object' ? stringify(value, newSpaceCount) : value;
-      return `${' '.repeat(newSpaceCount)}    ${key}: ${newValue}`;
-    }).join('\n');
+    .map(([key, value]) => (`${genSpaces(depth)}    ${key}: ${value}`)).join('\n');
 
-  return `{\n${entriesString}\n${' '.repeat(newSpaceCount)}}`;
+  return `{\n${entriesString}\n${genSpaces(depth)}}`;
 };
 
-const buildLine = (type, key, value, spaceCount, delValue, addValue) => {
-  if (type === 'changed') {
-    const deletedLine = buildLine('deleted', key, delValue, spaceCount);
-    const addedLine = buildLine('added', key, addValue, spaceCount);
-    return [deletedLine, addedLine];
-  }
-  return `${' '.repeat(spaceCount)}  ${signList[type]} ${key}: ${stringify(value, spaceCount)}`;
+const buildLine = (depth, sign, key, value) => {
+  const newValue = stringify(value, depth + 1);
+  return `${genSpaces(depth)}  ${sign} ${key}: ${newValue}`;
 };
 
-const visualRender = (astDiff, spaceCount = 0) => {
+const visualActions = [
+  {
+    type: 'unchanged',
+    build: ({ depth, key, value }) => buildLine(depth, ' ', key, value),
+  },
+  {
+    type: 'added',
+    build: ({ depth, key, value }) => buildLine(depth, '+', key, value),
+  },
+  {
+    type: 'deleted',
+    build: ({ depth, key, value }) => buildLine(depth, '-', key, value),
+  },
+  {
+    type: 'nested',
+    build: ({ key, depth, visualRender, children }) => {
+      const newValue = visualRender(children, depth + 1);
+      return `${genSpaces(depth)}    ${key}: ${newValue}`;
+    },
+  },
+  {
+    type: 'changed',
+    build: ({
+      depth, key, addedValue, deletedValue,
+    }) => {
+      const deletedLine = buildLine(depth, '-', key, deletedValue);
+      const addedLine = buildLine(depth, '+', key, addedValue);
+      return [deletedLine, addedLine];
+    },
+  },
+];
+
+const getVisualAction = nodeType => visualActions.find(({ type }) => nodeType === type);
+
+const visualRender = (astDiff, depth = 0) => {
   const resultString = astDiff
-    .map((node) => {
-      const {
-        key, value, type, children, deletedValue, addedValue,
-      } = node;
-      const newValue = type === 'nested' ? visualRender(children, spaceCount + 4) : value;
-      return buildLine(type, key, newValue, spaceCount, deletedValue, addedValue);
+    .map(({ type, ...rest }) => {
+      const { build } = getVisualAction(type);
+      return build({
+        type, visualRender, depth, ...rest,
+      });
     });
 
   const joinedResult = flatten(resultString).join('\n');
-  return `{\n${joinedResult}\n${' '.repeat(spaceCount)}}`;
+  return `{\n${joinedResult}\n${genSpaces(depth)}}`;
 };
 
 export default visualRender;
