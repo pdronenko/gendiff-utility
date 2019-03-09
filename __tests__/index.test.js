67a895cdd3f021e04847427f88c5a4ea308f086f
pdronenko <pdronenko@ya.ru>
2019-03-09 09:54:14 +0300

    refactor tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,20 +6,15 @@ const pathToInitialAfter = '__tests__/__fixtures__/initial/after';
 const extnameTable = [['.json'], ['.yml'], ['.ini']];
 
 describe('gendiff', () => {
-  const expectedJSONDiff = fs.readFileSync('__tests__/__fixtures__/expectedJSONResult', 'UTF-8');
+  const expectedDefaultDiff = fs.readFileSync('__tests__/__fixtures__/expectedDefaultResult', 'UTF-8');
   const expectedPlainDiff = fs.readFileSync('__tests__/__fixtures__/expectedPlainResult', 'UTF-8');
 
   test.each(extnameTable)('json test %s', (extname) => {
     const pathToBeforeFile = `${pathToInitialBefore}${extname}`;
     const pathToAfterFile = `${pathToInitialAfter}${extname}`;
-    const receivedDiff = gendiff(pathToBeforeFile, pathToAfterFile, 'default');
-    expect(receivedDiff).toBe(expectedJSONDiff);
-  });
-
-  test.each(extnameTable)('plain test %s', (extname) => {
-    const pathToBeforeFile = `${pathToInitialBefore}${extname}`;
-    const pathToAfterFile = `${pathToInitialAfter}${extname}`;
-    const receivedDiff = gendiff(pathToBeforeFile, pathToAfterFile, 'plain');
-    expect(receivedDiff).toBe(expectedPlainDiff);
+    const receivedDefaultDiff = gendiff(pathToBeforeFile, pathToAfterFile, 'default');
+    const receivedPlainDiff = gendiff(pathToBeforeFile, pathToAfterFile, 'plain');
+    expect(receivedDefaultDiff).toBe(expectedDefaultDiff);
+    expect(receivedPlainDiff).toBe(expectedPlainDiff);
   });
 });
