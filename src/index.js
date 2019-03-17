d09f38d3bdf105b8a84acbff955db2872ad7e6b7
pdronenko <pdronenko@ya.ru>
2019-03-17 17:35:20 +0300

    change const names

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import buildASTdiff from './buildASTdiff';
 const readDataFromFile = pathToFile => fs.readFileSync(pathToFile, 'UTF-8');
 
 export default (pathToFile1, pathToFile2, format = 'visual') => {
-  const beforeData = parse(readDataFromFile(pathToFile1), path.extname(pathToFile1));
-  const afterData = parse(readDataFromFile(pathToFile2), path.extname(pathToFile2));
-  const astDiff = buildASTdiff(beforeData, afterData);
+  const obj1 = parse(readDataFromFile(pathToFile1), path.extname(pathToFile1));
+  const obj2 = parse(readDataFromFile(pathToFile2), path.extname(pathToFile2));
+  const astDiff = buildASTdiff(obj1, obj2);
 
   return render(astDiff, format);
 };
